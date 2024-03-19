6166a11026620242795b924bb63f87768a5a9e9b
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2024-03-19 23:16:14 +0600

    added irtype and had to sacrifice the IrParser.c file :(

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
 
+#define IrType int
 #include "IrParser.h"
 
 int main(void) {    
     IrParser *irParser = createIrParser("parse-table.txt");
-
     irParse(irParser);
 
     destroyIrParser(irParser);
